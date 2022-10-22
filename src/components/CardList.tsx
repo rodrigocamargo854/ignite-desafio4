18bec2230e57a056fe3a8f5087f9feb5e8c2d8d9
Rodrigo Camargo <rodrigocamargo854@gmail.com>
2022-10-22 07:24:26 -0300

    map images uploaded working on index

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,16 +17,29 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
-
+  const { isOpen, onOpen, onClose } = useDisclosure();
   // TODO SELECTED IMAGE URL STATE
-
+  const [currentImageUrl, setCurrentImageUrl] = useState('');
   // TODO FUNCTION HANDLE VIEW IMAGE
+
+  function handleViewImage(url: string): void {
+    onOpen();
+    setCurrentImageUrl(url);
+  }
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid column={[1, 2, 3]} spacing="40px">
+        {cards.map(card => (
+          <Card data={card} viewImage={handleViewImage} key={card.id} />
+        ))}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        imgUrl={currentImageUrl}
+        isOpen={isOpen}
+        onClose={onClose}
+      />
     </>
   );
 }
